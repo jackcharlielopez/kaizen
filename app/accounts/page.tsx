725d7d1a7885f1bd77c672e4c066624521f64d2b
Jack Lopez <jlopez1992@yahoo.com>
2023-10-17 19:31:34 -0500

    adding parent and students profile picker page

--- app/accounts/page.tsx
+++ app/accounts/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { Box } from "@mantine/core";
+import { Avatar, Box, Group } from "@mantine/core";
 import { trpc } from "../_trpc/client";
+import { useSession } from "next-auth/react";
 
 // this will be netflix style profile creation and choosing
 // this should skip if only profile that exists is parents
@@ -9,8 +10,29 @@ import { trpc } from "../_trpc/client";
 
 const SelectProfile = () => {
   const { data } = trpc.getStudents.useQuery();
+  const { data: session, status } = useSession();
 
-  return <Box>Profile choose page</Box>;
+  console.log(session, data, status);
+
+  const getInitials = (name: string) => {
+    const splitName = name.split(" ");
+    return splitName[0].charAt(0).concat(splitName[1].charAt(0));
+  };
+
+  if (status === "loading") return <Box>Loading...</Box>;
+
+  return (
+    <Group justify="center" gap="xl">
+      <Avatar variant="filled" radius="sm" size="xl">
+        {getInitials(session?.user.name)}
+      </Avatar>
+      {data?.map((item) => (
+        <Avatar key={item.id} variant="filled" radius="sm" size="xl">
+          {getInitials(item.name)}
+        </Avatar>
+      ))}
+    </Group>
+  );
 };
 
 export default SelectProfile;
