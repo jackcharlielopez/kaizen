6997ca91541ccfac44376ed496d9e739a61bf332
Jack Lopez <jlopez1992@yahoo.com>
2023-10-25 21:43:09 -0500

    updating to add in calls to open ai api

--- app/_components/Student.tsx
+++ app/_components/Student.tsx
@@ -1,18 +1,36 @@
-import { Box, Flex, Paper, Text } from "@mantine/core";
-import { useContext } from "react";
+import { Box, Flex, Group, Paper, Stack, Text } from "@mantine/core";
+import { useContext, useState } from "react";
 import { AccountContext } from "../accounts/layout";
 import { Timer, TimerStatus } from "./Timer";
+import { trpc } from "../_trpc/client";
 
 const Student = () => {
+  let message = {
+    role: "user",
+    content:
+      'You are a tutor for an elementary school. Provide a math question to test my ability. Make sure each math question is formatted as "x + y = z"',
+  };
+  let messages = [];
+
+  messages.push(message);
+
   let { id } = useContext(AccountContext);
+  const [timerStat, setTimerStat] = useState(TimerStatus.disabled);
+  const { data } = trpc.setOpenAiMessage.useQuery(messages);
+
+  console.log(data);
 
   return (
-    <Flex justify={"center"} align={"center"} h={"calc(100vh - 165px"}>
-      <Paper w={"65%"} shadow="md" withBorder p="xl" h={"50%"}>
-        <Text>flash card</Text>
-        <Timer lengthOfTime={60} status={TimerStatus.start}></Timer>
-      </Paper>
-    </Flex>
+    <Stack justify="flex-start" gap={0}>
+      <Group justify="flex-end">
+        <Timer lengthOfTime={1200} status={timerStat}></Timer>
+      </Group>
+      <Group justify="center" h={500}>
+        <Paper w={"60%"} shadow="md" withBorder p="xl" h={"70%"}>
+          <Text>flash card</Text>
+        </Paper>
+      </Group>
+    </Stack>
   );
 };
 
--- server/router.ts
+++ server/router.ts
@@ -1,6 +1,8 @@
 import { prisma } from "@/lib/prisma";
 import { protectedProcedure, publicProcedure, router } from "./trpc";
 import { z } from "zod";
+import openai from "@/lib/openai";
+import { ChatCompletionMessage } from "openai/resources/chat/index.mjs";
 
 export const appRouter = router({
   getStudents: protectedProcedure
@@ -21,6 +23,28 @@ export const appRouter = router({
           userId: ctx.session?.user.id,
         },
       });
+    }),
+  setOpenAiMessage: protectedProcedure
+    .input(
+      z.array(
+        z.object({
+          role: z.string(),
+          content: z.string(),
+        })
+      )
+    )
+    .output(
+      z.object({
+        response: z.string().nullable(),
+      })
+    )
+    .query(async ({ input }) => {
+      const res = await openai.chat.completions.create({
+        model: "gpt-4",
+        messages: input as ChatCompletionMessage[],
+      });
+
+      return { response: res.choices[0].message.content };
     }),
 });
 
